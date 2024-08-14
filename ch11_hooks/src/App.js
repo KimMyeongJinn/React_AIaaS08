760fd0bd5b79dce765a465dd64154c890e7dc64e
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-14 11:14:53 +0900

    Hook - useState

---
+++ ch11_hooks/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import Counter from "./ch01_useState/Counter";
+import Info from "./ch02_useState/Info";
+
+function App() {
+  // return <Counter />;
+
+  return <Info />;
+}
+
+export default App;
