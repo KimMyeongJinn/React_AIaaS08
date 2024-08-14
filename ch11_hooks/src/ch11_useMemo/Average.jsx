39c729b05c350211d775b5f0adec5ba4578b0bbe
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-14 14:35:55 +0900

    useMemo의 필요성, 주석 추가

--- ch11_hooks/src/ch11_useMemo/Average.jsx
+++ ch11_hooks/src/ch11_useMemo/Average.jsx
@@ -31,6 +31,16 @@ const Average = () => {
         })}
       </ul>
       <div>
+        {/*         
+        getAverage는 list가 변했을 때만 평균을 계산하기 위해 호출되는 것이 좋다.
+        하지만 list, number 모두 useState로 만들어졌기 때문에
+        number가 변해도 Re-Rendering이 발생하므로
+        불필요하게 아래 getAverage(list)도 호출되게 된다.
+        Application이 만들어질 때 수많은 Component의 조합으로 구성되는데
+        Component들의 작은 성능 차이는 결국 전체 Application의 차이를
+        가져오게 된다.
+        이럴 때 useMemo를 사용하면 성능을 향상할 수 있다. 
+        */}
         <b>평균값: </b> {getAverage(list)}
       </div>
     </div>
