e9e47461f823f1876b631bba0c9d21c8af3d117a
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-14 17:19:12 +0900

    styled-component 사용법

---
+++ ch12_styled/src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import ReactButton from "./ex01_non_styled/ReactButton";
+// import StyledButton from "./ex02_styled/StyledButton";
+// import SimpleButton from "./ex03_styled/SimpleButton";
+// import SimpleButton from "./ex04_styled/SimpleButton";
+// import SimpleButton from "./ex05_styled/SimpleButton";
+// import LargeButton from "./ex05_styled/LargeButton";
+// import PrimaryButton from "./ex06_styled/PrimaryButton";
+// import PrimaryButton from "./ex07_styled/PrimaryButton";
+// import PrimaryButton from "./ex08_styled/PrimaryButton";
+import PrimaryButton from "./ex09_styled/PrimaryButton";
+import SecondButton from "./ex09_styled/SecondButton";
+
+function App() {
+  // return <ReactButton>안녕하세요</ReactButton>;
+  // return <StyledButton>감사합니다</StyledButton>;
+  // return <SimpleButton>감사합니다</SimpleButton>;
+  /*
+  return (
+    <>
+      <SimpleButton>행복합니다</SimpleButton>
+      <LargeButton>존경합니다</LargeButton>
+    </>
+  );
+  */
+  /*
+  return (
+    <>
+      <PrimaryButton primary>사랑합니다</PrimaryButton>
+      <PrimaryButton>고맙습니다</PrimaryButton>
+    </>
+  );
+  */
+  return (
+    <>
+      <PrimaryButton primary>사랑합니다</PrimaryButton>
+      <PrimaryButton>고맙습니다</PrimaryButton>
+      <SecondButton primary>Happy~</SecondButton>
+      <SecondButton>Good!</SecondButton>
+    </>
+  );
+}
+
+export default App;
---
+++ ch12_styled/src/ex02_styled/StyledButton.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+// js의 style말고, css의 style대로 설정
+const StyledButton = styled.button`
+  color: white;
+  background-color: purple;
+  margin: 10px;
+`;
+
+export default StyledButton;
