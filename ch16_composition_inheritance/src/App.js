c20334d1345466fb73dbdfc0e7ce59ceb11a3ffc
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-16 18:16:25 +0900

    설계방법 중 포함 예시

---
+++ ch16_composition_inheritance/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import WelcomeDesc from "./ex01_containment/WelcomeDesc";
+import WelcomeDialog from "./ex01_containment/WelcomeDialog";
+
+function App() {
+  return (
+    <>
+      <WelcomeDesc />
+      <WelcomeDialog />
+    </>
+  );
+}
+
+export default App;
