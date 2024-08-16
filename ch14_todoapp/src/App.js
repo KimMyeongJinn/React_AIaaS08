673b0977dde065e90121bda4e926620003f59dcf
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-16 12:40:46 +0900

    TodoList와 TodoListItem 컴포넌트 진행중...

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -2,14 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 // import TodoTemplate from "./ex01_TodoTemplate/TodoTemplate";
 // import TodoTemplate from "./ex02_TodoTemplate/TodoTemplate";
-import TodoTemplate from "./ex03_TodoInsert/TodoTemplate";
-import TodoInsert from "./ex03_TodoInsert/TodoInsert";
+// import TodoTemplate from "./ex03_TodoInsert/TodoTemplate";
+// import TodoInsert from "./ex03_TodoInsert/TodoInsert";
+import TodoTemplate from "./ex04_TodoList/TodoTemplate";
+import TodoInsert from "./ex04_TodoList/TodoInsert";
+import TodoList from "./ex04_TodoList/TodoList";
 
 function App() {
+  /*
+  return (
+    <TodoTemplate>
+      <div>TodoInsert</div>
+      <div>TodoList</div>
+    </TodoTemplate>
+  );
+  */
   return (
     <TodoTemplate>
       <TodoInsert />
-      <div>TodoList</div>
+      <TodoList />
     </TodoTemplate>
   );
 }
---
+++ ch14_todoapp/src/ex04_TodoList/TodoListItem.jsx
@@ -0,0 +1,9 @@
+const TodoListItem = () => {
+  return (
+    <div>
+      <div>할 일</div>
+    </div>
+  );
+};
+
+export default TodoListItem;
