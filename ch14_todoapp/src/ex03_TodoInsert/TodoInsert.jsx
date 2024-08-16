b014b0fc169d99fc97108008d9f1b1965f40d567
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-16 12:33:00 +0900

    TodoInsert 컴포넌트 생성

--- ch14_todoapp/src/ex03_TodoInsert/TodoInsert.jsx
+++ ch14_todoapp/src/ex03_TodoInsert/TodoInsert.jsx
@@ -1,5 +1,16 @@
+import { MdAdd, MdAddCircleOutline } from "react-icons/md";
+import "./TodoInsert.scss";
+
 const TodoInsert = () => {
-  return null;
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAdd />
+        {/* <MdAddCircleOutline /> */}
+      </button>
+    </form>
+  );
 };
 
 export default TodoInsert;
