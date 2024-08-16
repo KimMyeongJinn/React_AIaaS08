c3f73b3be25fadbaa1109fa32a06eb75f6b36b67
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-16 15:58:32 +0900

    onToggle 기능 완료

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -29,9 +29,15 @@ import TodoInsert from "./ex07_onInsert/TodoInsert";
 import TodoList from "./ex07_onInsert/TodoList";
 import { useState, useRef, useCallback } from "react";
 */
+/*
 import TodoTemplate from "./ex08_onRemove/TodoTemplate";
 import TodoInsert from "./ex08_onRemove/TodoInsert";
 import TodoList from "./ex08_onRemove/TodoList";
+import { useState, useRef, useCallback } from "react";
+*/
+import TodoTemplate from "./ex09_onToggle/TodoTemplate";
+import TodoInsert from "./ex09_onToggle/TodoInsert";
+import TodoList from "./ex09_onToggle/TodoList";
 import { useState, useRef, useCallback } from "react";
 
 function App() {
@@ -133,6 +139,7 @@ function App() {
   */
 
   // ex08_onRemove
+  /*
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -175,6 +182,64 @@ function App() {
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} onRemove={onRemove} />
+    </TodoTemplate>
+  );
+  */
+
+  // ex09_onToggle
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 해보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어 보기",
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  const onRemove = useCallback(
+    (id) => setTodos(todos.filter((todo) => todo.id !== id)),
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      );
+    },
+    [todos]
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
