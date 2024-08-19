a183f868bad37498d84ec0d2434346baa867bda5
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-19 15:36:00 +0900

    routing에 필요한 컴포넌트 작성

--- ch18_mini_blog/src/App.js
+++ ch18_mini_blog/src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 // import MiniBlogApp from "./ex01_folder/MiniBlogApp";
 // import MiniBlogApp from "./ex02_base_components/MiniBlogApp";
-import MiniBlogApp from "./ex03_routes/MiniBlogApp";
+// import MiniBlogApp from "./ex03_routes/MiniBlogApp";
+import MiniBlogApp from "./ex04_main_page/MiniBlogApp";
 
 function App() {
   return <MiniBlogApp />;
---
+++ ch18_mini_blog/src/ex04_main_page/page/MainPage.jsx
@@ -0,0 +1,5 @@
+const MainPage = (props) => {
+  return "MainPage 입니다";
+};
+
+export default MainPage;
