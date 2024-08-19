28f79989261da380e7b2df47eb501485550c7524
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-19 16:46:05 +0900

    PostViewPage 컴포넌트 작성

--- ch18_mini_blog/src/App.js
+++ ch18_mini_blog/src/App.js
@@ -7,7 +7,8 @@ import "./App.css";
 // import MiniBlogApp from "./ex05_main_page/MiniBlogApp";
 // import MiniBlogApp from "./ex06_post_list/MiniBlogApp";
 // import MiniBlogApp from "./ex07_post_list_item/MiniBlogApp";
-import MiniBlogApp from "./ex08_post_write_page/MiniBlogApp";
+// import MiniBlogApp from "./ex08_post_write_page/MiniBlogApp";
+import MiniBlogApp from "./ex09_post_view_page/MiniBlogApp";
 
 function App() {
   return <MiniBlogApp />;
