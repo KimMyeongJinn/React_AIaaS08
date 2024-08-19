a3f24f0a24f233495f717fdbbbf96c37b3ac012e
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-19 17:55:10 +0900

    컨텍스트 영역에 객체 저장

--- ch19_context/src/App.js
+++ ch19_context/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainApp from "./ex01_hierachy/MainApp";
+// import MainApp from "./ex01_hierachy/MainApp";
+// import MainApp from "./ex02_divide_files/MainApp";
+import MainApp from "./ex03_context/MainApp";
 
 function App() {
   return <MainApp />;
