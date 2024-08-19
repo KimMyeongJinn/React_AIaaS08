c6ba2851b27a4f80dce9f98b16ecc0a6ae3928f3
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-19 11:02:09 +0900

    리액트에서의 포함, 구체화, 상속 설계 기법

--- ch16_composition_inheritance/src/App.js
+++ ch16_composition_inheritance/src/App.js
@@ -3,9 +3,13 @@ import "./App.css";
 
 // import WelcomeDesc from "./ex01_containment/WelcomeDesc";
 // import WelcomeDialog from "./ex01_containment/WelcomeDialog";
-import SplitPane from "./ex02_containment/SplitPane";
-import WelcomeDesc from "./ex02_containment/WelcomeDesc";
-import WelcomeDialog from "./ex02_containment/WelcomeDialog";
+// import SplitPane from "./ex02_containment/SplitPane";
+// import WelcomeDesc from "./ex02_containment/WelcomeDesc";
+// import WelcomeDialog from "./ex02_containment/WelcomeDialog";
+import AlarmDialog from "./ex03_specialization/AlarmDialog";
+import WelcomeDialog from "./ex03_specialization/WelcomeDialog";
+import ProfileCard from "./ex04_inheritance/ProfileCard";
+import CookCard from "./ex04_inheritance/CookCard";
 
 function App() {
   /*
@@ -16,7 +20,7 @@ function App() {
     </>
   );
   */
-
+  /*
   return (
     <SplitPane
       npx={7}
@@ -24,6 +28,21 @@ function App() {
       left={<WelcomeDesc npx={3} color="red" />}
       right={<WelcomeDialog npx={3} color="blue" />}
     />
+  );
+  */
+  /*
+  return (
+    <>
+      <AlarmDialog />
+      <WelcomeDialog />
+    </>
+  );
+  */
+  return (
+    <>
+      <ProfileCard />
+      <CookCard />
+    </>
   );
 }
 
---
+++ ch16_composition_inheritance/src/ex04_inheritance/Card.jsx
@@ -0,0 +1,25 @@
+const Card = (props) => {
+  const { title, backgroundColor, children } = props;
+
+  return (
+    <div
+      style={{
+        margin: 8,
+        padding: 8,
+        borderRadius: 8,
+        boxShadow: "0px 0px 4px gray",
+
+        /* Specialization 기법 */
+        backgroundColor: backgroundColor || "white",
+      }}
+    >
+      {/* Specialization 기법 */}
+      {title && <h1>{title}</h1>}
+
+      {/* Containment 기법 */}
+      {children}
+    </div>
+  );
+};
+
+export default Card;
