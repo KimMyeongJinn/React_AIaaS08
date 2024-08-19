4e93755b4dd9813a0adb6c36a8493a94222db288
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-19 17:58:50 +0900

    useContext 사용해서 객체 꺼내기

--- ch19_context/src/App.js
+++ ch19_context/src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 // import MainApp from "./ex01_hierachy/MainApp";
 // import MainApp from "./ex02_divide_files/MainApp";
-import MainApp from "./ex03_context/MainApp";
+// import MainApp from "./ex03_context/MainApp";
+import MainApp from "./ex04_use_context/MainApp";
 
 function App() {
   return <MainApp />;
